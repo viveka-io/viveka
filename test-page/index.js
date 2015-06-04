af2dd7deecca2065e3511a54b8a4c5fdd8610828
Igor Mucsicska <mucsi_96@yahoo.com>
2015-06-04 08:38:15 +0200

    [Test page] templates added

--- test-page/index.js
+++ test-page/index.js
@@ -1,10 +1,14 @@
-var express = require('express');
+var express = require('express'),
+    connect_handlebars = require('connect-handlebars'),
     app = express(),
     port = 5556;
 
 app.use(express.static(__dirname + '/public'));
 
-app.use('/bower_components',  express.static(__dirname + '/../bower_components'));
+app.use('/bower_components', express.static(__dirname + '/../bower_components'));
+app.use("/js/templates.js", connect_handlebars(__dirname + "/templates", {
+    exts: ['hbs']
+}));
 app.listen(port, function() {
     console.log('Test page server is listening at %s', port);
 });
---
+++ test-page/public/js/app.js
@@ -0,0 +1,1 @@
+$('body').append(Handlebars.templates.a({title: 'template test'}));
