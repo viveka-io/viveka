8def8dcdcea744cb7f8850192aeb363c2029cb47
Krisztian Nagy <Krisztian_Nagy@epam.com>
2015-09-07 15:51:54 +0200

    test list page update

---
+++ admin/test-list/test-list.js
@@ -0,0 +1,25 @@
+var socket = io();
+
+Promise.promisifyAll(socket, {promisifier: function (originalMethod) {
+    return function promisified() {
+        var args = [].slice.call(arguments),
+            self = this;
+
+        return new Promise(function(resolve, reject) {
+            args.push(resolve);
+            originalMethod.apply(self, args);
+        });
+    };
+}});
+
+function getTests() {
+    socket.emitAsync('tests list', {})
+        .then(function(data){
+            var $tests = $(Handlebars.templates.tests(data));
+
+            console.log(data);
+            $tests.prependTo('#view');
+        });
+}
+
+getTests();
