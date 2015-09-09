b5542fa1e20b8095208fc3c487701a5dd4b25ae3
TWSgit <tws@cthul.hu>
2015-09-09 11:44:25 +0200

    Add jquery.panzoom bower component Style diff page templates

--- admin/diff-page/diff-page.js
+++ admin/diff-page/diff-page.js
@@ -34,3 +34,30 @@
 
     init();
 })();
+
+
+$(function() {
+    var $panzoom = $('.panzoom');
+
+    $panzoom.panzoom({
+        startTransform: 'scale(1)',
+        increment: 0.01,
+        minScale: 0.2,
+        maxScale: 1,
+        $zoomIn: $('#zoom-in'),
+        $zoomOut: $('#zoom-out'),
+        $zoomRange: $('#zoom'),
+        $reset: $('#zoom-reset'),
+        $set: $panzoom,
+        onZoom: function(e, panzoom, scale) {
+            $("#zoom")[0].MaterialSlider.change(scale);
+        },
+        onReset: function(e, panzoom, matrix) {
+            $("#zoom")[0].MaterialSlider.change(1);
+        }
+    });
+
+    $(window).on('resize', function() {
+        $panzoom.panzoom('resetDimensions');
+    });
+});
