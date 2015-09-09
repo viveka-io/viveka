45c5e9e8af1e50a4aea1d915aaa7d5cf71847ea0
Attila Bartha <v-abartha@hotels.com>
2015-09-09 11:10:52 +0200

    test details page functionalities

--- admin/test-details/test-details.js
+++ admin/test-details/test-details.js
@@ -11,13 +11,8 @@
 
     function init() {
         initRouter();
-
-        getTestDetailsAsync()
-            .then(loadTestDetailsView);
-
+        loadTestDetails();
         loadFingerprints();
-
-        attachEventHandlers();
     }
 
     function initRouter() {
@@ -25,10 +20,73 @@
         testId = router.getRoute()[0];
     }
 
+    function loadTestDetails() {
+        getTestDetailsAsync()
+            .then(renderTestDetailsView);
+    }
+
     function getTestDetailsAsync() {
         return socket.emitAsync('tests get', {
             id: testId
         });
+    }
+
+    function renderTestDetailsView(testDetails) {
+        var testData,
+            isNewTest = !!testDetails.error;
+
+        if (isNewTest) {
+            testData = {
+                browsers: mapBrowsers()
+            };
+        } else {
+            testData = testDetails.result;
+            testData.browsers = mapBrowsers(testData.config.browser);
+            $('#test-title').html(testData.name);
+        }
+
+        $('#add-fingerprint').attr('disabled', isNewTest);
+        $('#test-details-container').html(Handlebars.templates.testDetails(testData));
+        $('#add-test').toggleClass('hidden', !isNewTest);
+        $('#save-test').toggleClass('hidden', isNewTest);
+        componentHandler.upgradeAllRegistered();
+        attachTestDetailsEventHandlers();
+    }
+
+    function mapBrowsers(testBrowser) {
+        return browsers.map(function (browser) {
+            return {
+                name: browser.name,
+                iconname: browser.name.toLowerCase(),
+                value: browser.name.toUpperCase(),
+                selected: testBrowser && (browser.name.toUpperCase() === testBrowser.toUpperCase())
+            };
+        });
+    }
+
+    function attachTestDetailsEventHandlers() {
+        $('#add-test').off('click').on('click', createTest);
+    }
+
+    function createTest() {
+        var details = {
+                name: (new Date()).toISOString(),
+                config: {
+                    url: $('#url').val(),
+                    browserWidth: $('#browser-width').val(),
+                    browserHeight: $('#browser-height').val(),
+                    browser: $('input[name="browser"]').val(),
+                    generator: 'SENSE'
+                }
+            };
+
+        socket.emitAsync('tests create', details)
+            .then(reloadAfterCreateTest);
+    }
+
+    function reloadAfterCreateTest(newTestData) {
+        window.location.href = '/test-details.html#' + newTestData.result._id;
+        window.location.reload();
     }
 
     function loadFingerprints() {
@@ -42,44 +100,25 @@
         });
     }
 
-    function loadTestDetailsView(testDetails) {
-        var testData;
-
-        if (testDetails.error) {
-            testData = { browsers: browsers };
-        } else {
-            testData = testDetails.result;
-            setSelectedBrowser(testData.config.browser);
-            testData.browsers = browsers;
-            $('#test-title').html(testData.name);
-        }
-
-        $('#test-details-container').html(Handlebars.templates.testDetails(testData));
-        componentHandler.upgradeAllRegistered();
-    }
-
     function renderFingerprintsListView(testFingerprints) {
-        console.log(testFingerprints);
         $('#fingerprints-list-container').html(Handlebars.templates.fingerprintList(testFingerprints.result));
         componentHandler.upgradeAllRegistered();
+        attachFingerprintListEventHandlers();
     }
 
-    function setSelectedBrowser(testBrowser) {
-        browsers.forEach(function (browser) {
-            if (browser.name.toUpperCase() === testBrowser.toUpperCase()) {
-                browser.selected = true;
-            }
-            browser.iconname = browser.name.toLowerCase();
-        });
-    }
-
-    function attachEventHandlers() {
-        $('#add-fingerprint').on('click', createFingerprint);
+    function attachFingerprintListEventHandlers() {
+        $('#add-fingerprint').off('click').on('click', createFingerprint);
     }
 
     function createFingerprint() {
+        $('.fingerprint-list').toggleClass('disabled');
+        $('#add-fingerprint').attr('disabled', true);
         socket.emitAsync('fingerprints create', { id: testId })
-                .then(loadFingerprints);
+            .then(loadFingerprints)
+            .then(function () {
+                $('#add-fingerprint').attr('disabled', false);
+                $('.fingerprint-list').toggleClass('disabled');
+            });
     }
 
     Promise.promisifyAll(socket, {
