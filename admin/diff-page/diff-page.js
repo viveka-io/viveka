3b4feea09ffdb338f588275baf32c98d785772e1
Attila Bartha <v-abartha@hotels.com>
2015-09-09 14:46:15 +0200

    differences page js

--- admin/diff-page/diff-page.js
+++ admin/diff-page/diff-page.js
@@ -6,16 +6,83 @@
         router,
         testId,
         baselineId,
-        currentId;
+        currentId,
+        fingerprintA,
+        fingerprintB;
 
     function init() {
         initRouter();
+        loadTestDetails();
+        loadDifferences();
     }
 
     function initRouter() {
         router = new Router();
-        baselineId = router.getRoute()[0];
-        currentId = router.getRoute()[1];
+        testId = router.getRoute()[0];
+        baselineId = router.getRoute()[1];
+        currentId = router.getRoute()[2];
+    }
+
+    function loadTestDetails() {
+        getTestDetailsAsync()
+            .then(renderTestDetailsView);
+    }
+
+    function getTestDetailsAsync() {
+        return socket.emitAsync('tests get', {
+            id: testId
+        });
+    }
+
+    function renderTestDetailsView(testDetails) {
+        $('#test-details-container').html(Handlebars.templates.testDetails(testDetails.result));
+        componentHandler.upgradeAllRegistered();
+    }
+
+    function panzoomify() {
+        var $panzoom = $('.panzoom');
+
+        $panzoom.each(function () {
+            $(this).panzoom({
+                startTransform: 'scale(1)',
+                increment: 0.01,
+                minScale: 0.2,
+                maxScale: 1,
+                $zoomIn: $('#zoom-in'),
+                $zoomOut: $('#zoom-out'),
+                $zoomRange: $('#zoom'),
+                $reset: $('#zoom-reset'),
+                $set: $panzoom,
+                onZoom: function (e, panzoom, scale) {
+                    $('#zoom')[0].MaterialSlider.change(scale);
+                },
+                onReset: function (e, panzoom, matrix) {
+                    $('#zoom')[0].MaterialSlider.change(1);
+                }
+            });
+        });
+
+        $(window).on('resize', function () {
+            $panzoom.panzoom('resetDimensions');
+        });
+    }
+
+    function loadDifferences() {
+        getDifferencesAsync()
+            .then(renderDifferencesView);
+    }
+
+    function getDifferencesAsync() {
+        return socket.emitAsync('differences create json', {
+            baselineId: baselineId,
+            targetId: currentId
+        });
+    }
+
+    function renderDifferencesView(differencesData) {
+        $('#diff-tool-container').html(Handlebars.templates.diffTool(differencesData));
+        panzoomify();
+        componentHandler.upgradeAllRegistered();
     }
 
     Promise.promisifyAll(socket, {
@@ -34,30 +101,3 @@
 
     init();
 })();
-
-
-$(function() {
-    var $panzoom = $('.panzoom');
-
-    $panzoom.panzoom({
-        startTransform: 'scale(1)',
-        increment: 0.01,
-        minScale: 0.2,
-        maxScale: 1,
-        $zoomIn: $('#zoom-in'),
-        $zoomOut: $('#zoom-out'),
-        $zoomRange: $('#zoom'),
-        $reset: $('#zoom-reset'),
-        $set: $panzoom,
-        onZoom: function(e, panzoom, scale) {
-            $("#zoom")[0].MaterialSlider.change(scale);
-        },
-        onReset: function(e, panzoom, matrix) {
-            $("#zoom")[0].MaterialSlider.change(1);
-        }
-    });
-
-    $(window).on('resize', function() {
-        $panzoom.panzoom('resetDimensions');
-    });
-});
