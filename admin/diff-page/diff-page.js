54c702382f43b63bf26e59ec7df35d8400ef7935
TWSgit <tws@cthul.hu>
2015-09-21 17:15:21 +0200

    Constrain screenshot movements

--- admin/diff-page/diff-page.js
+++ admin/diff-page/diff-page.js
@@ -77,6 +77,40 @@ function panzoomify() {
             $zoomRange: $('#zoom'),
             $reset: $('#zoom-reset'),
             $set: $panzoom,
+            onChange: function (e, panzoom, transform) {
+                var contentRect = this.getBoundingClientRect(),
+                    parentRect = this.parentNode.getBoundingClientRect(),
+                    isWider = contentRect.width > parentRect.width,
+                    isTaller = contentRect.height > parentRect.height,
+                    limited = false,
+                    scale = transform[0];
+
+                if (isWider && contentRect.left > parentRect.left || !isWider && contentRect.left < parentRect.left) {
+                    transform[4] = ( contentRect.width - contentRect.width / scale ) / 2;
+                    limited = true;
+                }
+                if (isWider && contentRect.right < parentRect.right || !isWider && contentRect.right > parentRect.right) {
+                    transform[4] = parentRect.width - contentRect.width - ( contentRect.width / scale - contentRect.width ) / 2;
+                    limited = true;
+                }
+
+                if (isTaller && contentRect.top > parentRect.top || !isTaller && contentRect.top < parentRect.top) {
+                    transform[5] = ( contentRect.height - contentRect.height / scale ) / 2;
+                    limited = true;
+                }
+                if (isTaller && contentRect.bottom < parentRect.bottom || !isTaller && contentRect.bottom > parentRect.bottom) {
+                    transform[5] = parentRect.height - contentRect.height - ( contentRect.height / scale - contentRect.height ) / 2;
+                    limited = true;
+                }
+
+                if (limited) {
+                    panzoom.setMatrix(transform, {
+                        animate: false,
+                        range: true,
+                        silent: true
+                    });
+                }
+            },
             onZoom: function (e, panzoom, scale) {
                 $('#zoom')[0].MaterialSlider.change(scale);
             },
@@ -87,6 +121,11 @@ function panzoomify() {
     });
 
     $(window).on('resize', function () {
+        var $baselineContainer = $('#baseline-container'),
+            $baselineParent = $baselineContainer.parent(),
+            minScale = $baselineParent.width() / $baselineContainer.width();
+
+        $panzoom.panzoom('option', 'minScale', minScale);
         $panzoom.panzoom('resetDimensions');
     });
 }
