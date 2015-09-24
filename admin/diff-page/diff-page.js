81855429588826d9126f070ecd0ab22c9d8d09fa
TWSgit <tws@cthul.hu>
2015-09-24 11:07:04 +0200

    Remove panzoom

--- admin/diff-page/diff-page.js
+++ admin/diff-page/diff-page.js
@@ -63,86 +63,6 @@ function declineFingerprint(jQueryEvent) {
         });
 }
 
-function panzoomify() {
-    var $panzoom = $('.panzoom');
-
-    $panzoom.each(function () {
-        $(this).panzoom({
-            startTransform: 'scale(1)',
-            increment: 0.01,
-            minScale: 0.2,
-            maxScale: 1,
-            $zoomIn: $('#zoom-in'),
-            $zoomOut: $('#zoom-out'),
-            $zoomRange: $('#zoom'),
-            $reset: $('#zoom-reset'),
-            $set: $panzoom,
-            onChange: function (e, panzoom, transform) {
-                // TODO: handle all panzoom view
-                var contentRect = this.getBoundingClientRect(),
-                    parentRect = this.parentNode.getBoundingClientRect(),
-                    isWider = contentRect.width > parentRect.width,
-                    isTaller = contentRect.height > parentRect.height,
-                    limited = false,
-                    scale = transform[0];
-
-                if (isWider && contentRect.left > parentRect.left || !isWider && contentRect.left < parentRect.left) {
-                    transform[4] = ( contentRect.width - contentRect.width / scale ) / 2;
-                    limited = true;
-                }
-                if (isWider && contentRect.right < parentRect.right || !isWider && contentRect.right > parentRect.right) {
-                    transform[4] = parentRect.width - contentRect.width - ( contentRect.width / scale - contentRect.width ) / 2;
-                    limited = true;
-                }
-
-                if (isTaller && contentRect.top > parentRect.top || !isTaller && contentRect.top < parentRect.top) {
-                    transform[5] = ( contentRect.height - contentRect.height / scale ) / 2;
-                    limited = true;
-                }
-                if (isTaller && contentRect.bottom < parentRect.bottom || !isTaller && contentRect.bottom > parentRect.bottom) {
-                    transform[5] = parentRect.height - contentRect.height - ( contentRect.height / scale - contentRect.height ) / 2;
-                    limited = true;
-                }
-
-                if (limited) {
-                    panzoom.setMatrix(transform, {
-                        animate: false,
-                        range: true,
-                        silent: true
-                    });
-                }
-            },
-            onZoom: function (e, panzoom, scale) {
-                $('#zoom')[0].MaterialSlider.change(scale);
-            },
-            onReset: function () {
-                // TODO: handle all panzoom view
-                var scale = Math.min($panzoom.parent().height() / $panzoom.height(), $panzoom.parent().width() / $panzoom.width()),
-                    scaledWidth = $(this).width() * scale,
-                    scaledHeight = $(this).height() * scale,
-                    tX = ( scaledWidth - scaledWidth / scale ) / 2,
-                    tY = ( scaledHeight - scaledHeight / scale ) / 2,
-                    matrix = [scale, 0, 0, scale, tX, tY];
-
-                $panzoom.panzoom('setMatrix', matrix, {
-                    animate: true,
-                    range: true
-                });
-                $('#zoom')[0].MaterialSlider.change(scale);
-            }
-        });
-    });
-
-    $(window).on('resize', function () {
-        var $baselineContainer = $('#baseline-container'),
-            $baselineParent = $baselineContainer.parent(),
-            minScale = $baselineParent.width() / $baselineContainer.width();
-
-        $panzoom.panzoom('option', 'minScale', minScale);
-        $panzoom.panzoom('resetDimensions');
-    });
-}
-
 function loadDifferences() {
     getDifferencesAsync()
         .then(renderDifferencesView);
@@ -157,10 +77,14 @@ function getDifferencesAsync() {
     });
 }
 
+function initLeaflet() {
+
+}
+
 function renderDifferencesView(differencesData) {
     $('#diff-tool-container').html(Handlebars.templates.diffTool(differencesData));
     componentHandler.upgradeAllRegistered();
-    panzoomify();
+    initLeaflet();
 }
 
 init();
